fb02e76322bf3d4a86b63d400188a9ac420940d0
Kamal <dragkamal71@gmail.com>
2024-09-25 19:14:01 +0530

    changes added

--- apps/user-app/app/lib/actions/p2pTransfer.ts
+++ apps/user-app/app/lib/actions/p2pTransfer.ts
@@ -22,7 +22,7 @@ export async function p2pTransfer(to: string, amount: number) {
             message: "User not found"
         }
     }
-    await prisma.$transaction(async (tx) => {
+    await prisma.$transaction(async (tx : any) => {
         // To lock the row until one transaction completes.
         await tx.$queryRaw `SELECT * FROM "Balance" WHERE "userId" = ${Number(from)} FOR UPDATE` 
         const fromBalance = await tx.balance.findUnique({
